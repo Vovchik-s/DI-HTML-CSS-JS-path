20ad24327e885394014371f4ab623e6b793e3459
Vladimir <vladimirmizkevich@gmail.com>
2022-06-09 14:43:53 +0300

    day-4-Puzzle

---
+++ week-10/day-4/Puzzle.sql
@@ -0,0 +1,37 @@
+
+Questions
+Q1. What will be the OUTPUT of the following statement?
+
+    SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+	
+	Anwswer: 3
+	Output: 0
+
+
+Q2. What will be the OUTPUT of the following statement?
+
+    SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+	
+	Anwswer: 2
+	Output: 2
+
+
+Q3. What will be the OUTPUT of the following statement?
+
+    SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+	
+	Anwswer: 2
+	Output: 0
+
+
+Q4. What will be the OUTPUT of the following statement?
+
+    SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+											
+												
+	Anwswer: 3
+	Output: 2

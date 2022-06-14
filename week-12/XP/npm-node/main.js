3227af229e90f855f85a9af75b7184c8ff8a7d32
Vladimir <vladimirmizkevich@gmail.com>
2022-06-14 12:37:09 +0300

    day-2-XP

---
+++ week-12/XP/npm-node/main.js
@@ -0,0 +1,24 @@
+// Exercise 1 : Hello Node.Js
+// Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+
+// let a = 5;
+// let b = 10;
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+
+// sum(a,b)
+
+
+// Exercise 2 : Hello Node.Js #2
+
+// Exercise 3 : Nodemon
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log("Second run:",x + c)
